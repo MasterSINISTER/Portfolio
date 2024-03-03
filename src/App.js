a240579c51ebb57688c167e4ca185614452a66b9
MasterSINISTER <hellolucifer007@gmail.com>
2024-03-03 15:26:41 +0530

    Updated the Nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/Navbar';
 import Home from './component/Home';
 
 function App() {
+  
   return (
     <>
         <Home />
